7d0fde47cbeaa892bb50df4eab24ad7b5d226605
JIN JAE WOONG <johnjin56@gmail.com>
2024-09-02 06:13:47 +0000

    monday

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -96,6 +96,7 @@ struct thread
 	int original_priority;	   /* 원래 우선순위 */
 	struct list donators;	   /* 기부자들 명단 */
 	struct lock *waiting_lock; /* 기다리고 있는 락 */
+	int nice;				   /* 나이스값 */
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem; /* List element. */
